32cd28cb849685e681a735643fa1511ba2ab4e0a
Sidd <siddz415@gmail.com>
2023-02-27 22:04:26 -0800

    filled readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Notepad
+
+## Description
+This is a note taker app with starter code that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+
+## Github Repo Link
+https://github.com/siddz415/Notepad
+
+## Github Pages Link
+https://siddz415.github.io/Notepad/
+
+## Screenshot
+![Screenshot of the Site](./assets/note.png) 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.port || 3001;
 
 const app = express();
@@ -15,34 +16,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.get('/api/notes', (req, res) => {
     fs.promises.readFile('db/db.json')
-    .then((data)=> {
-        res.send(data); // return the data
-    })
+        .then((data) => {
+            res.send(data); // return the data
+        })
 })
 
-app.post('/api/notes', (req, res) =>{
+app.post('/api/notes', (req, res) => {
     const note = req.body;
+    note.id = uuidv4();
     fs.promises.readFile('db/db.json')
-    .then((data)=> {
-        const db = JSON.parse(data);
-        db.push(note);
-        return fs.promises.writeFile('db/db.json', JSON.stringify(db))
-        
-    })
-    .then((data)=> {
-        res.json(data);
-    })
+        .then((data) => {
+            const db = JSON.parse(data);
+            db.push(note);
+            return fs.promises.writeFile('db/db.json', JSON.stringify(db))
+
+        })
+        .then((data) => {
+            res.json(data);
+        })
+})
+app.delete('/api/notes/:id', (req, res) => {
+    const deleteId = req.params.id;
+    fs.promises.readFile('db/db.json')
+        .then((data) => {
+            const db = JSON.parse(data);
+            const filterNotes = db.filter((note) => {
+                console.log(note.id, deleteId);
+                return note.id !== deleteId;
+            })
+            console.log('filterNotes', filterNotes)
+            return fs.promises.writeFile('db/db.json', JSON.stringify(filterNotes))
+
+        })
+        .then((data) => {
+            res.send(data);
+        })
 })
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for feedback page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
